79717c24bf1899ef1f445df894c1d5e0e9856d32
tigreeroo <ganz67@mail.ru>
2025-09-05 23:30:15 +0300

    add sourcetype to init file

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,9 +1,11 @@
 from .base import Base
 from .item import Item
 from .answer import Answer
+from .enums import SourceType
 
 __all__ = (
     'Base',
     'Item',
-    'Answer'
+    'Answer',
+    'SourceType',
 )
