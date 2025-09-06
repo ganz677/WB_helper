d440d93cb3373064f1c2894f324759786bf99e7d
tigreeroo <ganz67@mail.ru>
2025-09-06 14:21:46 +0300

    add db_helper to init

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,10 +2,12 @@ from .base import Base
 from .item import Item
 from .answer import Answer
 from .enums import SourceType
+from .db_helper import db_helper
 
 __all__ = (
     'Base',
     'Item',
     'Answer',
     'SourceType',
+    'db_helper'
 )
