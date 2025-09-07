912df5fc3008f9ddea47c8de79644e7e7316bb8d
tigreeroo <ganz67@mail.ru>
2025-09-07 15:33:27 +0300

    add wb router to v1 router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
+from app.api.v1.wb_feedback_autoanswers.routes import router as wb_router
+
 
 router = APIRouter(
     prefix='/v1'
 )
+
+router.include_router(wb_router)
